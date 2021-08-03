a427d5294c72617f26415a961317df0b3bcfcd2c
kenta0313 <asanokenta@me.com>
2021-08-03 17:42:45 +0900

    usesiginin

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -5,7 +5,7 @@ import { FcGoogle } from 'react-icons/fc';
 import Navbar from '../common/Navbar';
 import { useAuth } from '../lib/auth';
 
-const signin = () => {
+const useSignin = () => {
   const { auth, siginWithGoogle } = useAuth();
   const router = useRouter();
 
@@ -32,4 +32,4 @@ const signin = () => {
   );
 };
 
-export default signin;
+export default useSignin;
